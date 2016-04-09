15748ad989a44b76b0b61f6a09f18a6421ea6d70
kevguy <kevlai22@uw.edu>
2016-04-10 05:31:25 +0800

    feat: add code to show CreatedOn date

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@
     this.review = {};
     
     this.addReview = function(product){
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
